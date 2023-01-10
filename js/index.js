b6996e90be9ea5b342bbed796dc31c37269fdd26
GuillermoBaldan <guillermobaldan@gmail.com>
2023-01-10 07:01:24 +0100

    Lección 18 - Menu desplegable

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$navsetup = document.querySelector("#setup");
+$navconceptos = document.querySelector("#conceptos");
+
+$navsetup.addEventListener('click', handleNavSetup)
+$navconceptos.addEventListener('click', handleNavConceptos)
+
+function handleNavSetup(){
+    console.log("Has pulsado Setup")
+    if($navconceptos.open){
+        $navconceptos.open = false;
+    }
+}
+
+function handleNavConceptos(){
+    console.log("Has pulsado conceptos")
+    if($navsetup.open){
+        $navsetup.open = false;
+    }
+}
+
+console.log("Hola js")
